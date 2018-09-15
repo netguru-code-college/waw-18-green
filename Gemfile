e1580baddb07f4c1b52a321252bf789bd0fc898d
jjkazm <jjkazm@gmail.com>
2018-09-15 15:41:46 +0200

    fix seed.rb file

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'bootstrap', '~> 4.1.3'
 gem 'sprockets-rails'
 gem 'devise'
+gem 'faker'
 
 group :development, :test do
   gem 'pry-rails'
@@ -35,7 +36,7 @@ group :test do
   gem 'capybara', '>= 2.15', '< 4.0'
   gem 'chromedriver-helper'
   gem 'factory_bot_rails'
-  gem 'faker'
+
   gem 'rspec-rails'
   gem 'selenium-webdriver'
   gem 'shoulda-matchers'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: "Marek", email: "marek@wp.pl", password: "haslo123")
-Bar.create!(name: "asd", address: "adres", description: "opis", rating: 4.0)
+user = User.create!(username: Faker::Internet.username, email: Faker::Internet.email, password: "haslo123")
 
+Bar.create!(name: "asd", address: "adres", description: "opis", rating: 4.0, user_id: user.id)
