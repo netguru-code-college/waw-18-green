f40b4f4575c03298fece6fe29662ebeeaa3d2429
jjkazm <jjkazm@gmail.com>
2018-09-15 15:16:52 +0200

    user-bar association

--- app/views/bars/_bar.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! bar, :id, :name, :address, :description, :rating, :user_id, :created_at, :updated_at
-json.url bar_url(bar, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_091623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +37,5 @@ ActiveRecord::Schema.define(version: 2018_09_15_091623) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
