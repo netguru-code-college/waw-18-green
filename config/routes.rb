6a8b34956f5881cc218cef071bc6a6a04064fbdf
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2018-09-15 11:28:28 +0200

    generated user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'devise/registrations#new' #TODO: change after chinczyk initialization
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
