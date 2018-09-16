ce3b5ea7f40954b63cee55f52eb8cdd8ce83a96c
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2018-09-16 13:40:23 +0200

    pagination on user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController<ApplicationController
   def show
     @user = User.find_by(id: params[:id])
-    @bars = BarDecorator.decorate_collection(Bar.where(user_id: params[:id]).order(rating: :desc))
+    @bars = BarDecorator.decorate_collection(Bar.where(user_id: params[:id]).order(rating: :desc).paginate(page: params[:page], per_page: 5))
   end
 end
