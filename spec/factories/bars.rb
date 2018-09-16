fd75cc62dbb7f59bf72d87e6568725d25807beb0
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2018-09-16 11:24:31 +0200

    configured factory bot in rspec

--- spec/factories/bars.rb
+++ spec/factories/bars.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :bar do
-    name "Chinczyk z Hali Mirowskiej"
+    name "Chinczyk z Mirowskiej"
     address "Some address"
     description "some description is used here"
     rating 5
